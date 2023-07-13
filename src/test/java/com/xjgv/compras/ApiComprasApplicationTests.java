2bc0beed696f80f28ea4f398ae06261a2ef15ba9
Javier Gomez <javier.gov@outlook.es>
2023-07-13 11:50:08 -0600

    Creacion del proyecto

---
+++ src/test/java/com/xjgv/compras/ApiComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xjgv.compras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

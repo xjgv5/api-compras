85db5e482f7e1e7a9f2d6c8bf7269c2382dde473
Javier Gomez <javier.gov@outlook.es>
2023-07-18 00:01:54 -0600

    Archivo de configuracion para produccion y desarrollo

---
+++ src/main/java/com/xjgv/compras/HolaMundoController.java
@@ -0,0 +1,15 @@
+package com.xjgv.compras;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludar")
+public class HolaMundoController {
+
+    @GetMapping("/hola")
+    public String saludar(){
+        return "malditas frases de motivacion";
+    }
+}
